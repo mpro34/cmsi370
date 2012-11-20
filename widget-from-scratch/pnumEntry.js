a11f34ee0fd261102fefa145f58b8fa6efa4e61f
mpro34 <mpro34@gmail.com>
2012-11-20 15:57:33 -0800

    Changed the number creation to string concatenation instead ofchanging arrays

--- widget-from-scratch/pnumEntry.js
+++ widget-from-scratch/pnumEntry.js
@@ -1,89 +1,89 @@
-var numarray = new Array();
+var numarray = "";
 var count = 0;
 var display = function () {
 	count++;
-    console.log(count);
+  //  console.log(count);
 	/*Add hyphen between numbers automatically*/
     if (count==3) {
-        numarray[count] = "-";
+        numarray=numarray.concat("-");
     }
 	if (count==7) {
-		numarray[count] = "-";
+		numarray=numarray.concat("-");
 	}
-	/*Erase current phone number and start new one*/
+	/*Erase current phone number and start new one
 	if (count==11) {
 		count=0;
 		numstring=numarray.toString();
 		numarray=numstring.replace(/,/g,"");
-		/*numarray.splice(0,11);*/
+		/*numarray.splice(0,11);
 	}
-
+            */
     document.getElementById("result").innerHTML = numarray;
 };
 
-document.getElementById("newNum").onclick = function () {
+/*document.getElementById("newNum").onclick = function () {
     numarray=numstring.split(" ");
     numarray.splice(0,11);
     display()
-};
+};*/
 
 document.getElementById("b1").onclick = function () {
-    numarray.push("1");
+    numarray=numarray.concat("1");
     display();
 };
 
 document.getElementById("b2").onclick = function () {
-    numarray.push("2");
+    numarray=numarray.concat("2");
     display();
 };
 
 document.getElementById("b3").onclick = function () {
-    numarray.push("3");
+    numarray=numarray.concat("3");
     display();
 };
 
 document.getElementById("b4").onclick = function () {
-    numarray.push("4");
+    numarray=numarray.concat("4");
     display();
 };
 
 document.getElementById("b5").onclick = function () {
-    numarray.push("5");
+    numarray=numarray.concat("5");
     display();
 };
 
 document.getElementById("b6").onclick = function () {
-    numarray.push("6");
+    numarray=numarray.concat("6");
     display();
 };
 
 document.getElementById("b7").onclick = function () {
-    numarray.push("7");
+    numarray=numarray.concat("7");
     display();
 };
 
 document.getElementById("b8").onclick = function () {
-    numarray.push("8");
+    numarray=numarray.concat("8");
     display();
 };
 
 document.getElementById("b9").onclick = function () {
-    numarray.push("9");
+    numarray=numarray.concat("9");
     display();
 };
 
 document.getElementById("b0").onclick = function () {
-    numarray.push("0");
+    numarray=numarray.concat("0");
     display();
 };
 
 document.getElementById("star").onclick = function () {
-    numarray.push("*");
+    numarray=numarray.concat("*");
     display();
 };
 
 document.getElementById("pound").onclick = function () {
-    numarray.push("#");
+    numarray=numarray.concat("#");
     display();
 };
 

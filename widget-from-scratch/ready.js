f1a8ea693c2b2370398c7dc00a3c55745b4d8f9c
mpro34 <mpro34@gmail.com>
2012-12-01 17:59:23 -0800

    Added css code for a bad, good, and great password

--- widget-from-scratch/ready.js
+++ widget-from-scratch/ready.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+
 	var count = $("div").length;
+
 	$("div").append($("<input/>", {'type' : "text"}));
 	for (var i=0; i<count; i++) {		//Goes through each div and looks up its id.
        var id = $("input:eq("+i+")").closest("div").attr("id");
@@ -8,17 +10,46 @@ $(document).ready(function(){
 	//Each time the user changes the password field, the following will determine its security level.
 	
 	$("div").keyup(function(){
+        var isBad = false;
+        var isGood = false;
+        var isGreat = false;
 	    var password = $(this).children().attr("value");
-	    alert("password: "+password);
-		if (password == "a") {
-		    alert("success");
+
+	    //alert("password: "+password);
+	    for (var i=0; i<password.length; i++) {
+	    	//Check if numbers are present in the password
+	    	if (!isNaN(password.charAt(i)) {
+	    		isGood = true;
+	    	}
+	    	//Check if capital letters are present in the password
+	    	
+	    }
+
+
+	/*    var intRegex = /^\d+$/;
+        var floatRegex = /^((\d+(\.\d *)?)|((\d*\.)?\d+))$/;
+
+       // var str = $('#myTextBox').val();
+        if(intRegex.test(password) || floatRegex.test(password)) {
+            alert('I am a number');
+        }*/
+
+
+	  //Check if the password is code-red (bad password)
+	  //Only lowercase letters
+		if (isBad) {
+		    $(this).children().addClass("code-red");
+	    }
+	  //Check if the password is code-yellow (good password)
+	  //lowercase letters and numbers
+        if (isGood) {
+		    $(this).children().addClass("code-yellow");
+	    }
+
+	  //Check if the password is code-green (great password)
+	  //lowercase letters, numbers, and uppercase letters
+	    if (isGreat) {
+		    $(this).children().addClass("code-green");
 	    }
 	});
-
-   /* $("div").keyup(function(){
-    alert($(this).children().attr("value"));
-    });*/
-    
-	
-
 })

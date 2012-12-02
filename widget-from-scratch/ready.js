3fb060d9bfb9ec9953644b29e6c9386f58eb5024
mpro34 <mpro34@gmail.com>
2012-12-02 15:49:47 -0800

    Fixed an error where the password strength check values would remain the same, no matter which input field is typed in. However, the check values only update properly if there is an alert statement, need to fix

--- widget-from-scratch/ready.js
+++ widget-from-scratch/ready.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
 	var count = $("div").length;
-
-	$("div").append($("<input/>", {'type' : "text"}));
+    //The name attribute in each input field will keep track of the checks below.
+	$("div").append($("<input/>", {'type' : "text", 'name' : "000"}));
 	for (var i=0; i<count; i++) {		//Goes through each div and looks up its id.
        var id = $("input:eq("+i+")").closest("div").attr("id");
        $("input:eq("+i+")").attr("id", id);   //Takes that id and sets the id of the input tag.
@@ -12,53 +12,48 @@ $(document).ready(function(){
 	var isBad = 0;
     var isGood = 0;
     var isGreat = 0;
+    var check = "";
 	$("div").keyup(function(){
 		//If isGood or isGreat is greater than 1, then that property applies to the current pw.
-     
-        
+		if ($(this).children().attr("name") != check) {
+			isBad = 0;
+            isGood = 0;
+            isGreat = 0;
+            current = 0;
+		}
 	    var password = $(this).children().attr("value");
-	    //Check if capital letters and numbers are present in the password
+	    //HERE
+	    alert(!isNaN(password.charAt(current)));
+	    //Check if capital letters are present in the password
 	    	if ((/[A-Z]/.test(password.charAt(current)))) {
 	    		isGreat++;
-	    	//	alert("great");
 	    	}
 	    	//Check if numbers are present in the password
 	    	else if (!isNaN(password.charAt(current))) { 
-	    	//	alert("good");
 	    		isGood++;
 	    	}
-	    	else {
+	        else {
                 isBad++;
-            //    alert("bad");
 	    	}
+	 
+
 	   // alert("here:" + (password.charAt(current)));
 	    current++;        //Update the current pw character value
-	   
-	    //alert("password: "+password);
-	   // for (var i=0; i<password.length; i++) {
-
-	    	//alert(password.charAt(i+current));
-	    	//current++;
-	   // }
-
-	/*    var intRegex = /^\d+$/;
-        var floatRegex = /^((\d+(\.\d *)?)|((\d*\.)?\d+))$/;
-
-       // var str = $('#myTextBox').val();
-        if(intRegex.test(password) || floatRegex.test(password)) {
-            alert('I am a number');
-        }*/
+	  //Remove current class so that a new class may be assigned.
+	   $(this).children().removeClass("code-green").removeClass("code-yellow").removeClass("code-red");
       //Check if the password is code-green (great password)
 	  //lowercase letters, numbers, and uppercase letters
-	  $(this).children().removeClass("code-green").removeClass("code-yellow").removeClass("code-red");
 	    if (isGreat > 0 && isGood > 0 && isBad > 0) {
 		    $(this).children().addClass("code-green");
 	    }
 
 	  //Check if the password is code-yellow (good password)
-	  //lowercase letters and numbers
+	  //lowercase letters or uppercase letters and numbers
         else if (isGreat == 0  && isGood > 0 && isBad > 0) {
 		    $(this).children().addClass("code-yellow");
+	    }
+	    else if (isGreat > 0  && isGood > 0 && isBad == 0) {
+	    	$(this).children().addClass("code-yellow");
 	    }
 
 	  //Check if the password is code-red (bad password)
@@ -66,7 +61,11 @@ $(document).ready(function(){
 		else {
 		    $(this).children().addClass("code-red");
 	    }
-	  	//alert("1: "+isGreat+" 2: "+isGood+" 3: "+isBad);
+
+	  //Update check values
+	    check = ("").concat(isBad.toString()).concat(isGood.toString()).concat(isGreat.toString()); 
+	    $(this).children().attr("name", check);
+	  	
 
 
 	});

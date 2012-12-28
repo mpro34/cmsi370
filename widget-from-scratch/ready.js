a211e46776de553d177478f4b93ac613d8cb76f8
dondi <dondi@lmu.edu>
2012-12-27 23:12:46 -0800

    Updated feedback for HW 1204 based on 12/13 commits, with accompanying update to standards achievement report.

--- widget-from-scratch/ready.js
+++ widget-from-scratch/ready.js
@@ -9,13 +9,28 @@ $(document).ready(function() {
     }
 
     //Each time the user changes the password field, the following will determine its security level.
+    // JD: As far as I can tell, none of these variables are used outside
+    //     of the event handler function.  Thus, they are declared at the
+    //     wrong level (too high up).  Bad habit---it does not overtly
+    //     harm you here, but may in future, more complex work.
     var current = 0;
     var isBad = 0;
     var isGood = 0;
     var isGreat = 0;
     var check = "";
+
+    // JD: You addressed the "every single div" problem above by using
+    //     a ps_field class, which is good, but then you repeat that mistake
+    //     below, by binding "every single input" to have password-strength
+    //     behavior.  There may be further input elements on the web page that
+    //     are not meant to be password strength indicators.
     $("input").bind("input", function(event){
+        // JD: This code is inside the event handler function, and therefore
+        //     should be indented!  Sloppy.
     //If isGood or isGreat is greater than 1, then that property applies to the current pw.
+    // JD: The prefix "is" implies that a variable is boolean (i.e., either it
+    //     "is" or "is not").  And yet these variables are integers, which you
+    //     increment based on various conditions.  Bad naming choice.
     isBad = 0;
     isGood = 0;
     isGreat = 0;
@@ -40,6 +55,8 @@ $(document).ready(function() {
 
         //Check if there are lower case letters present in the password
         if (/[a-z]/.test(password)) {
+            // JD: So...isBad is not necessarily bad?  You *really* need better
+            //     variable names.
             isBad++;
         }
 	   
@@ -57,7 +74,9 @@ $(document).ready(function() {
         else if (isGood && isBad) {
             $(this).addClass("code-yellow");
 	    }
-
+            // JD: Some code duplication here.  Both blocks above and below
+            //     add the code-yellow class.  They can be collapsed into a
+            //     single clause.
         else if (isGreat && isGood) {
             $(this).addClass("code-yellow");
         }
@@ -69,9 +88,12 @@ $(document).ready(function() {
         }
 
         //Update check values
+        // JD: I see why you did this, but in the end I think this is mainly
+        //     debug code.  Removable once the widget works as intended.
         check = ("").concat(isBad.toString()).concat(isGood.toString()).concat(isGreat.toString()); 
         $(this).attr("name", check);
         //Removes the default code-red class if there is no password string.
+        // JD: Note again, === is preferred in JavaScript.
         if (password.length == 0) {
             $(this).removeClass("code-red");
         }
